0a664a774b57c231ebc20c379c9ec607f13d4a55
Biprodeep Bose <bipbose123@gmail.com>
2025-12-18 16:39:26 +0530

    update

--- server.js
+++ server.js
@@ -10,11 +10,20 @@ const routeRoutes = require("./routes/routeRoutes");
 const { setupSockets } = require("./services/socketService");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mapgoex.vercel.app"],
+    methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://mapgoex.vercel.app"],
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use("/api/simulate", simulationRoutes(io, config));
 app.use("/api/route", routeRoutes(config));
